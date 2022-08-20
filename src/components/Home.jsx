bca0b92bc0959763afa14fd212b4906706e97a24
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-08-21 01:26:49 +1200

    third commit on the website

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,32 @@ import pictwo from "../images/HeadersPhotos/P1080200p1.jpg";
 import picthree from "../images/HeadersPhotos/P1080210p1.jpg";
 
 export default function HomePage() {
+  var instagramLink = "https://www.instagram.com/kali.graphy/";
   return (
     <div>
       <div className="header--div">
         <img className="picone" alt="first pic" src={picone} />
         <img className="pictwo" alt="second pic" src={pictwo} />
         <img className="picthree" alt="third pic" src={picthree} />
-        <h1 className="welcome--sign">Kaliphotography</h1>
+        <h1 className="welcome--sign">Kali Photography</h1>
+
+        <p className="intro"></p>
+
+        <div>
+          <ul className="contact-icon">
+            <li className="icon">
+              <a className="anchor" href={instagramLink} target="_blank">
+                <i class="inverted instagram icon"></i>
+              </a>
+            </li>
+            <li className="icon">
+              <i class="inverted whatsapp icon"></i>
+            </li>
+            <li className="icon">
+              <i class="inverted envelope outline icon"></i>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
